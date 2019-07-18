a4f7390d0b712a8318654116481975f0ffdf7fff
times1623 <c_lee23@fanshaweonline.ca>
2019-07-18 13:54:11 +0900

    website layout finish and some of them needs to be fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 (() => {
 console.log('stuff fired');
 
+//
+// $(".ham").click(function(){
+// 	$(this).toggleClass("active");
+// });
+
+var hamNav = document.querySelector('.ham');
+var hideNav = document.querySelector('.hiddenNav');
+
+function toggleNav(){
+  hamNav.classList.toggle('active');
+  if(hamNav.classList.contains('active')){
+    hideNav.classList.add('navMap');
+    hideNav.classList.remove('hiddenNav');
+  }else{
+    hideNav.classList.remove('navMap');
+    hideNav.classList.add('hiddenNav');
+  }
+  console.log(hamNav);
+}
+
+hamNav.addEventListener('click', toggleNav, false);
 
 
 
