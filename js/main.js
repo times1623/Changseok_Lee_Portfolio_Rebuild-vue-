7b3edd84a714c400790096534555d84c84bc90be
times1623 <c_lee23@fanshaweonline.ca>
2019-08-01 17:50:46 +0900

    scroll top function add and portfolio page build start

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ var hideNav = document.querySelector('.hiddenNav');
 var scDown = document.querySelector('#scrollD');
 var hotbod = document.querySelector("#headerInt");
 var togOn = document.querySelector('#tCon');
+var showWorks = document.querySelector('.portBut');
+var scrollTop = document.querySelector('#scTop');
+
+// console.log(scrollTop);
+
 
 function toggleNav(){
   hamNav.classList.toggle('active');
@@ -41,6 +46,19 @@ window.onload = function() {
   hotbod.classList.add('animate');
 };
 
+showWorks.addEventListener("click", function() {
+  window.location.href = "portfolio.html";
+
+});
+
+
+scrollTop.addEventListener("click", function() {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+
+  });
+});
 
 
 
